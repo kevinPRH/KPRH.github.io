d9154a0b98ac618293480a7fd838e85563f304d0
kevinPRH <128870861+kevinPRH@users.noreply.github.com>
2023-04-21 14:48:37 -0600

    proyecto

---
+++ js/Tipos_de_Datos.js
@@ -0,0 +1,50 @@
+//console.log("Hola");
+
+//Numerico
+
+var numero =17 ;
+document.write(numero);
+
+//String cedena de texto
+
+var nombre ="aqui ";
+var texto =" Algo \"Importante\"";
+document.write(nombre);
+document.write(texto);
+
+//Array Arreglos
+
+var amigos = ["harold"];
+amigos.push("");
+document.write(amigo);
+
+//Object
+
+var obj = {
+    nombre:"Nombre",
+    direccion:"Golondrinas",
+    telefono : 55-59-53-90-86,
+    C_P:14300,
+}
+document.write(obj.direccion);
+
+//Boolean
+
+var cierto = true;
+var falso = false;
+document.write(obj.direccion);
+
+//undefined
+
+var texto2;
+document.write(texto2);
+
+//NaN 
+
+var texto5 = "Lorem ipsum";
+document.write(texto5*2);
+
+//Null
+
+var numero5 ="50"
+numero5="60"
